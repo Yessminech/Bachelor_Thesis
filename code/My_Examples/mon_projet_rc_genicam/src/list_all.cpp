4e58513b967c408f57d763a036ceb96ccd9cd44b
Yessminech <chabchoub.yessmine@gmail.com>
2025-02-15 11:32:50 +0100

    listdevices Example complete

--- code/My_Examples/mon_projet_rc_genicam/src/list_all.cpp
+++ code/My_Examples/mon_projet_rc_genicam/src/list_all.cpp
@@ -6,61 +6,140 @@
 #include <iostream>
 #include <set>
 
+struct DeviceConfig
+{
+  std::string id;
+  std::string vendor;
+  std::string model;
+  std::string tlType;
+  std::string displayName;
+  std::string userDefinedName;
+  std::string accessStatus;
+  std::string serialNumber;
+  std::string version;
+  std::string currentIP;
+  std::string MAC;
+  double timestampFrequency;
+};
+
+//   bool deprecated = rcg::getString(nodemap, "GevCurrentIPAddress") != "" ? true : false;
+
+std::string getCurrentIP(const std::shared_ptr<rcg::Device> &device)
+{
+  device->open(rcg::Device::CONTROL);
+  auto nodemap = device->getRemoteNodeMap();
+  try
+  {
+    std::string currentIP = rcg::getString(nodemap, "GevCurrentIPAddress");
+    device->close();
+    return currentIP;
+  }
+  catch (const std::exception &ex)
+  {
+    device->close();
+    return "";
+  }
+}
+
+std::string getMAC(const std::shared_ptr<rcg::Device> &device)
+{
+  device->open(rcg::Device::CONTROL);
+  auto nodemap = device->getRemoteNodeMap();
+  try
+  {
+    std::string MAC = rcg::getString(nodemap, "GevMACAddress");
+    device->close();
+    return MAC;
+  }
+  catch (const std::exception &ex)
+  {
+    device->close();
+    return "";
+  }
+}
+DeviceConfig getConfig(const std::shared_ptr<rcg::Device> &device) 
+{ // ToDo: Exceptions handling
+  DeviceConfig config;
+  config.id = device->getID();
+  config.vendor = device->getVendor();
+  config.model = device->getModel();
+  config.tlType = device->getTLType();
+  config.displayName = device->getDisplayName();
+  config.userDefinedName = device->getUserDefinedName();
+  config.accessStatus = device->getAccessStatus();
+  config.serialNumber = device->getSerialNumber();
+  config.version = device->getVersion();
+  config.timestampFrequency = device->getTimestampFrequency();
+  config.currentIP = getCurrentIP(device);
+  config.MAC = getMAC(device);
+  return config;
+}
+
+bool listDevices()
+{ // ToDo: Exceptions handling
+  bool ret = true;
+  std::set<std::string> printedSerialNumbers;
+
+  // list all systems, interfaces and devices
+  std::vector<std::shared_ptr<rcg::System>> system = rcg::System::getSystems();
+  for (size_t i = 0; i < system.size(); i++)
+  {
+    system[i]->open();
+    std::vector<std::shared_ptr<rcg::Interface>> interf = system[i]->getInterfaces();
+    for (size_t k = 0; k < interf.size(); k++)
+    {
+      interf[k]->open();
+      std::vector<std::shared_ptr<rcg::Device>> device = interf[k]->getDevices();
+      for (size_t j = 0; j < device.size(); j++)
+      {
+        auto nodemap = device[j]->getRemoteNodeMap();
+        std::string serialNumber = device[j]->getSerialNumber();
+        if (printedSerialNumbers.find(serialNumber) == printedSerialNumbers.end())
+        {
+          printedSerialNumbers.insert(serialNumber);
+          DeviceConfig config = getConfig(device[j]);
+          std::cout << "        Device             " << config.id << std::endl;
+          std::cout << "        Vendor:            " << config.vendor << std::endl;
+          std::cout << "        Model:             " << config.model << std::endl;
+          std::cout << "        TL type:           " << config.tlType << std::endl;
+          std::cout << "        Display name:      " << config.displayName << std::endl;
+          std::cout << "        User defined name: " << config.userDefinedName << std::endl;
+          std::cout << "        Access status:     " << config.accessStatus << std::endl;
+          std::cout << "        Serial number:     " << config.serialNumber << std::endl;
+          std::cout << "        Version:           " << config.version << std::endl;
+          std::cout << "        TS Frequency:      " << config.timestampFrequency << std::endl;
+          std::cout << "        Current IP:        " << config.currentIP << std::endl;
+          std::cout << "        MAC:               " << config.MAC << std::endl;
+          std::cout << std::endl;
+        }
+        else
+        {
+          std::cout << "        Looking...        " << std::endl;
+        }
+      }
+      interf[k]->close();
+    }
+    system[i]->close();
+  }
+  return ret;
+}
+
 int main(int argc, char *argv[])
 {
-  int ret=0;
+  int ret = 0;
 
   try
   {
-        std::set<std::string> printedSerialNumbers;
-
-        // list all systems, interfaces and devices
-        std::vector<std::shared_ptr<rcg::System> > system=rcg::System::getSystems();
-        for (size_t i=0; i<system.size(); i++)
-        {
-          system[i]->open();            
-          std::vector<std::shared_ptr<rcg::Interface> > interf=system[i]->getInterfaces();
-            for (size_t k=0; k<interf.size(); k++)
-            {
-            interf[k]->open();
-            std::vector<std::shared_ptr<rcg::Device> > device=interf[k]->getDevices();
-            for (size_t j=0; j<device.size(); j++)
-            {
-              // device[j]->open(rcg::Device::CONTROL);
-              auto nodemap = device[j]->getRemoteNodeMap();
-              std::string serialNumber = device[j]->getSerialNumber();
-              if (printedSerialNumbers.find(serialNumber) == printedSerialNumbers.end())
-              {
-              printedSerialNumbers.insert(serialNumber);
-              std::cout << "        Device             " << device[j]->getID() << std::endl;
-              std::cout << "        Vendor:            " << device[j]->getVendor() << std::endl;
-              std::cout << "        Model:             " << device[j]->getModel() << std::endl;
-              // std::cout << "        IP:                " << rcg::getString(nodemap, "GevCurrentIPAddress") << std::endl;
-              std::cout << "        TL type:           " << device[j]->getTLType() << std::endl;
-              std::cout << "        Display name:      " << device[j]->getDisplayName() << std::endl;
-              std::cout << "        User defined name: " << device[j]->getUserDefinedName() << std::endl;
-              std::cout << "        Access status:     " << device[j]->getAccessStatus() << std::endl;
-              std::cout << "        Serial number:     " << serialNumber << std::endl;
-              std::cout << "        Version:           " << device[j]->getVersion() << std::endl;
-              std::cout << "        TS Frequency:      " << device[j]->getTimestampFrequency() << std::endl;
-              std::cout << std::endl;
-              // device[j]->close();
-              }
-              else
-              {
-              std::cout << "        Looking...        " << std::endl;
-              }
-            }
-            interf[k]->close();
-            }
-          system[i]->close();
-        }
-    }
-    catch (const std::exception &ex)
+    if (!listDevices())
     {
-        std::cerr << "Exception: " << ex.what() << std::endl;
-        ret=2;
+      ret = 1;
     }
+  }
+  catch (const std::exception &ex)
+  {
+    std::cerr << "Exception: " << ex.what() << std::endl;
+    ret = 2;
+  }
 
   rcg::System::clearSystems();
 
